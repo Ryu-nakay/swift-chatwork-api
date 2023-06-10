4840912f852ae5cf9de7e1ed3dd96f52dd6cd248
Ryu-nakay <ryukenkana@gmail.com>
2023-06-10 11:28:31 +0900

    ChatworkAPIにAPITokenを持たせた

--- Sources/SwiftChatworkAPI/SwiftChatworkAPI.swift
+++ Sources/SwiftChatworkAPI/SwiftChatworkAPI.swift
@@ -3,4 +3,16 @@
 
 struct ChatworkAPI {
     static let shared: ChatworkAPI = .init()
+    private init() {}
+    
+    private var token: APIToken?
+    
+    // ChatworkAPIのTokenを設定するメソッド
+    mutating func registerToken(token: String) throws {
+        do {
+            self.token = try APIToken(value: token)
+        } catch {
+            throw error
+        }
+    }
 }
