d2676e93495b1340d823642804ec523f110cbd60
Ryu-nakay <ryukenkana@gmail.com>
2023-06-10 16:36:24 +0900

    POSTのテストを書き、レッドを確認

--- Tests/SwiftChatworkAPITests/Repository/RoomList/RoomsRepositoryTests.swift
+++ Tests/SwiftChatworkAPITests/Repository/RoomList/RoomsRepositoryTests.swift
@@ -9,10 +9,19 @@ import XCTest
 @testable import SwiftChatworkAPI
 
 final class RoomsRepositoryTests: XCTestCase {
-    func test_ChatworkAPIへ正しいTokenでリクエストをするとRoomsGetResponse型のモデルが返ってくること() async throws {
+    let token = try! APIToken(value: "InputYourToken")
+    
+    func test_ChatworkAPIへ正しいTokenでGETリクエストをするとRoomsGetResponse型のモデルが返ってくること() async throws {
         let repository = RoomsRepository()
-        let result = try await repository.get(token: APIToken(value: "InputYourToken"))
+        let result = try await repository.get(token: token)
 
         XCTAssertTrue(result is RoomsGetResponse)
+    }
+    
+    func test_ChatworkAPIへ正しいTokenでPOSTリクエストをするとroom_idがInt型のモデルが返ってくること() async throws {
+        let repository = RoomsRepository()
+        let result = try await repository.post(token: token, formData: RoomsRepositoryPostFormData)
+
+        XCTAssertTrue(result is Int)
     }
 }
