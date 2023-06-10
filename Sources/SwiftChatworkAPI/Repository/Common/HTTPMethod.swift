f79a6484a1429607c9e9a04795283e4e7acb0753
Ryu-nakay <ryukenkana@gmail.com>
2023-06-10 15:46:46 +0900

    MeRepositoryを実装し、テストをグリーンにした

---
+++ Sources/SwiftChatworkAPI/Repository/Common/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by cw-ryu.nakayama on 2023/06/10.
+//
+
+import Foundation
+
+// リポジトリ共通で使うHTTPのメソッド(必要になったらメソッドは生やす)
+enum HTTPMethod: String {
+    case get = "GET"
+}
