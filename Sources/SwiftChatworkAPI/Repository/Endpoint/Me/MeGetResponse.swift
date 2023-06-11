475e64717c6b811392a092c26dfe703d81e9319b
Ryu-nakay <ryukenkana@gmail.com>
2023-06-11 09:44:52 +0900

    型名やテストを修正

--- Sources/SwiftChatworkAPI/Repository/Endpoint/Me/MeGetResponse.swift
+++ Sources/SwiftChatworkAPI/Repository/Endpoint/Me/MeGetResponse.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Me: Decodable {
+struct MeGetResponse: Decodable {
     let accountId: Int
     let roomId: Int
     let name: String
--- Tests/SwiftChatworkAPITests/Repository/Me/MeRepositoryTests.swift
+++ Tests/SwiftChatworkAPITests/Repository/Me/MeRepositoryTests.swift
@@ -12,11 +12,11 @@ final class MeRepositoryTests: XCTestCase {
     let token = try! APIToken(value: KeyManager().getAPIToken())
     
     // Meの返ってくることを確認できればOKとする
-    func test_ChatworkAPIへ正しいTokenでリクエストをするとMe型のモデルが返ってくること() async throws {
+    func test_ChatworkAPIへ正しいTokenでリクエストをするとMeGetResponse型のモデルが返ってくること() async throws {
         let repository = MeRepository()
         let result = try await repository.get(token: token)
         
-        XCTAssertTrue(result is Me) // 常にtrueだけど、テストの意図を伝えるための記述
+        XCTAssertTrue(result is MeGetResponse) // 常にtrueだけど、テストの意図を伝えるための記述
     }
     
     func test_ChatworkAPIへ間違ったTokenでリクエストをすると例外が返ってくること() async throws {
